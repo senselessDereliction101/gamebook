30557681f8a1a8c4d26876a3797b26267858ad68
senselessDereliction101 <32080161+senselessDereliction101@users.noreply.github.com>
2020-02-25 22:40:10 -0700

    Final Commit

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -1,5 +1,6 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 
 require __DIR__ . "/../src/Entity/Game.php";
 require __DIR__ . "/../src/Entity/Rating.php";
@@ -14,15 +15,31 @@ class GameTest extends TestCase {
    }
 
    public function testImage_WithPath_ReturnsPath() {
+        $game = new Game();
+        $game->setImagePath('images/game1.png');
+        Assert::assertEquals('images/game1.png', 
+        $game->getImagePath());
 
    }
 
    public function testAverageScore_WithoutRatings_ReturnsNull() {
-
+        $game = new Game();
+        $game->setRatings([]);
+        Assert::assertNull($game->getAverageScore());   
    }
 
    public function testAverageScore_With6And8_Returns7() {
+        $rating1 = $this->createMock(Rating::class);
+        $rating1->method('getScore')->willReturn(6);
 
+        $rating2 = $this->createMock(Rating::class);
+        $rating2->method('getScore')->willReturn(8);
+
+        $game = $this->getMockBuilder(Game::class)
+        ->setMethods(array('getRatings'))
+        ->getMock();
+        $game->method('getRatings')->willReturn([$rating1, $rating2]);
+        Assert::assertEquals(7, $game->getAverageScore());
    }
 
    public function testAverageScore_WithNullAnd5_Returns5()
@@ -43,9 +60,34 @@ class GameTest extends TestCase {
 
    public function testIsRecommended_WithCompatibility2AndScore10_ReturnsFalse()
    {
+        $rating1 = $this->createMock(Rating::class);
+        $rating1->method('getScore')->willReturn(10);
+        
+        $user = $this->createMock(User::class);
+        $user->method('getGenreCompatibility')->willReturn(2);
+
+        $game = $this->getMockBuilder(Game::class)
+          ->setMethods(array('getRatings'))
+          ->getMock();
+        $game->method('getRatings')->willReturn([$rating1]);
+    
+        self::assertFalse($game->isRecommended($user));
    }
 
    public function testIsRecommended_WithCompatibility10AndScore10_ReturnsTrue()
    {
+        $rating1 = $this->createMock(Rating::class);
+        $rating1->method('getScore')->willReturn(10);
+
+        $user = $this->createMock(User::class);
+        $user->method('getGenreCompatibility')->willReturn(10);
+
+        $game = $this->getMockBuilder(Game::class)
+         ->setMethods(array('getRatings'))
+         ->getMock();
+        $game->method('getRatings')->willReturn([$rating1]);
+
+        $this->assertTrue($game->isRecommended($user));
+
    }
 }
